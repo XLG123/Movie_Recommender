9b5b86ac88e09a8f09971104acf6291ab85d4062
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2022-03-14 21:05:26 -0400

    Initial Commit

---
+++ Movie_Recommender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_Recommender
+//
+//  Created by Fnu Tsering on 3/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
