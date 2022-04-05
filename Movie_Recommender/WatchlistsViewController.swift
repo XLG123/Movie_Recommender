fee48aef45c97407a8a3af9284eb01f74f83661f
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2022-04-05 17:13:09 -0400

    Revert "Merge branch 'main' of github.com:XLG123/Movie_Recommender into main"
    This reverts commit 6b05fe4e58155040ad4036020ae75f21ef6e86b5, reversing
    changes made to 8f7da5b92ecfe4d54b782968d17cfaf44e42a7c5.

--- Movie_Recommender/WatchlistsViewController.swift
+++ Movie_Recommender/WatchlistsViewController.swift
@@ -7,53 +7,15 @@
 
 import UIKit
 
-class WatchlistsViewController: UIViewController, UITableViewDataSource {
+class WatchlistsViewController: UIViewController {
 
-    // Create an array to store the movie lists
-    var movieLists = [MovieListItem]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.prefersLargeTitles = true
-//        navigationController?.navigationBar.backgroundColor = UIColor(red: 40/255.0, green: 60/255.0, blue: 84/255.0, alpha: 90) // Dark blue navigation bar color
-        
-        // Create the first object to be stored in the array
-        let movielist1 = MovieListItem()
-        movielist1.listTitle = "Watch List"
-        movieLists.append(movielist1)
-        
-        // Create the second object to be stored in the array
-        let movielist2 = MovieListItem()
-        movielist2.listTitle = "Watched List"
-        movieLists.append(movielist2)
-        
-        let movielist3 = MovieListItem()
-        movielist3.listTitle = "Likes List"
-        movieLists.append(movielist3)
+
         // Do any additional setup after loading the view.
     }
     
-    // MARK: - Showing the lists on Watchlists Screen
-    
-    // Get the number of lists in our array.
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieLists.count
-    }
 
-    // Show each list to the screen.
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "movielistitem", for: indexPath)
-        let movielist = movieLists[indexPath.row]
-
-        configureText(for: cell, with: movielist)
-        return cell
-    }
-    
-    func configureText(for cell: UITableViewCell, with item: MovieListItem) {
-        let label = cell.viewWithTag(20) as! UILabel
-        label.text = item.listTitle
-    }
-    
     /*
     // MARK: - Navigation
 
