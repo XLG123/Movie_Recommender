39d4081221bc9bbb640266ede89c0e63f1e92130
mickeyc0309 <chenmickey0309@gmail.com>
2022-05-03 14:53:53 -0400

    Revert "Add files via upload"
    This reverts commit ef14d306ebf94965a8ab51574e83593b8cca3a71.

--- Target Support Files/Pods-Movie_Recommender/Pods-Movie_Recommender-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Movie_Recommender : NSObject
-@end
-@implementation PodsDummy_Pods_Movie_Recommender
-@end
--- Target Support Files/Pods-Movie_RecommenderUITests/Pods-Movie_RecommenderUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Movie_RecommenderUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Movie_RecommenderUITests
-@end
