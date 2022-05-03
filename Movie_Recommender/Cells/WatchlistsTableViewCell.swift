d7c8c174b2c04b56bf16f5a49d625b631d80050c
mickeyc0309 <100549274+mickeyc0309@users.noreply.github.com>
2022-05-03 13:52:10 -0400

    Add files via upload

---
+++ Movie_Recommender/Cells/WatchlistsTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  WatchlistsTableViewCell.swift
+//  Movie_Recommender
+//
+//  Created by Xiao Lin Guan on 3/28/22.
+//
+import Foundation
+
+class MovieListItem {
+    var listTitle = ""
+}
